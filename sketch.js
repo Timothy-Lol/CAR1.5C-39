7d639bf695dbdf32b7d9dd2f568cd0421c9a3aeb
Timothy-Lol <7_99@bhssjr.com>
2021-05-27 20:43:57 +0530

    clas

--- sketch.js
+++ sketch.js
@@ -9,7 +9,17 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var groundImg, trackImg;
+var car1Img, car2Img, car3Img, car4Img;
 
+function preload(){
+  groundImg = loadImage("images/ground.png");
+  trackImg = loadImage("images/track.jpg")
+  car1Img = loadImage("images/car1.png");
+  car2Img = loadImage("images/car2.png");
+  car3Img = loadImage("images/car3.png");
+  car4Img = loadImage("images/car4.png");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -21,11 +31,15 @@ function setup(){
 
 
 function draw(){
+  
   if(playerCount === 4){
     game.update(1);
   }
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+    game.end();
   }
 }
